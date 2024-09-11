c72f79c04cebc60ecfa63d93d103a8842db170b3
RandilT <randiltharusha72@gmail.com>
2024-09-11 15:36:20 +0530

    Refactor icon and title in WhyChooseUs component

--- components/sections/why-choose-us.tsx
+++ components/sections/why-choose-us.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React from "react";
 import FadeInWhenVisible from "../fade-in-when-visible";
-import { Trophy, Users, Zap } from "lucide-react";
+import { CircleCheck, Cog, PiggyBank, Trophy, Users, Zap } from "lucide-react";
 import { motion } from "framer-motion";
+import { FaMoneyBill } from "react-icons/fa6";
+import { BiMoney } from "react-icons/bi";
 
 const WhyChooseUs = () => {
   return (
@@ -12,28 +14,28 @@ const WhyChooseUs = () => {
         <div className="container mx-auto">
           <FadeInWhenVisible>
             <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
-              Why Choose JDM?
+              Why Choose Us?
             </h2>
           </FadeInWhenVisible>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {[
               {
-                icon: Trophy,
-                title: "Unmatched Performance",
+                icon: CircleCheck,
+                title: "Quality Assurance",
                 description:
-                  "Experience cutting-edge engineering and speed that defines the JDM legacy.",
+                  "100% genuine vehicles and parts directly from Japan.",
               },
               {
-                icon: Zap,
-                title: "Iconic Designs",
+                icon: PiggyBank,
+                title: "Budget-Friendly",
                 description:
-                  "Own a piece of automotive history with timeless Japanese aesthetics.",
+                  "Competitive pricing and flexible financing options",
               },
               {
-                icon: Users,
-                title: "Passionate Community",
+                icon: Cog,
+                title: "Japanese Expertise",
                 description:
-                  "Join a global network of enthusiasts who share your love for JDM cars.",
+                  "Unparalleled selection of premium Japanese vehicles and machinery.",
               },
             ].map((item, index) => (
               <FadeInWhenVisible key={index}>
@@ -41,7 +43,7 @@ const WhyChooseUs = () => {
                   className="bg-gray-800 rounded-lg p-6 shadow-lg h-full"
                   whileHover={{ scale: 1.05 }}
                 >
-                  <item.icon className="w-12 h-12 mx-auto mb-4 text-red-500" />
+                  <item.icon className="w-16 h-16 mx-auto mb-4  text-red-500" />
                   <h3 className="text-xl font-semibold mb-2 text-center">
                     {item.title}
                   </h3>
